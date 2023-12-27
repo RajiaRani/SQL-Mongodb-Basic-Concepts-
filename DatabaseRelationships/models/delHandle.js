98f9b0e14da3fc5e1fc70110062daf6d96cbd4c9
RajiaRani <rajiarani.syal@gmail.com>
2023-12-26 18:16:57 -0600

    updated

--- DatabaseRelationships/models/delHandle.js
+++ DatabaseRelationships/models/delHandle.js
@@ -34,3 +34,51 @@ const postSchema = new Schema({
 
 const Instauser = mongoose.model("Instauser", userSchema);
 const Instapost = mongoose.model("Instapost",postSchema);
+
+const addData2 = async() => {
+    let user2 =  await Instauser.findOne({name:"Riya Rajput" });
+     let post2 = new Instapost ({
+        caption:"hello..Good Morning everyone :) ",
+        likes:10,
+        share:0,
+      mention:["@radhikasharma", "@pardeepsingh", "@anitachoudhary", "@rahulsharma"],
+     });
+
+    post2.user = user2;
+     await user2.save();
+};
+addData2();
+
+
+const addData = async() => {
+    let user1 = new Instauser ({
+        name:"Riya Rajput",
+        email:"riya123@gmail.com",
+        gender:"female",
+        age:23,
+    });
+    // let post = new Instapost ({
+    //     caption:"lets, start new journey :) ",
+    //     likes:102,
+    //     share:2,
+    //     mention:["@rohitsharma", "@helloworld", "@javascriptconcepts", "@rajiasyal", "@romasyal", "@i_amchenchureddy"],
+    // });
+
+    // post.user = user1;
+    let res = await user1.save();
+    console.log(res);
+};
+//addData();
+
+
+const addPost = async() => {
+    let post = new Instapost({
+        caption:"lets, start new journey :) ",
+        likes:102,
+       share:2,
+      mention:["@rohitsharma", "@helloworld", "@javascriptconcepts", "@rajiasyal", "@romasyal", "@i_amchenchureddy"],
+    });
+    await post.save();
+};
+
+//addPost();
