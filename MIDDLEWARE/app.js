0b1aa190ea81ad9c670f7ac099920d4ef3e7aef4
RajiaRani <rajiarani.syal@gmail.com>
2023-12-28 11:18:56 -0600

    update the app.js file

--- MIDDLEWARE/app.js
+++ MIDDLEWARE/app.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
 
-
-app.get("/", (req,res) => {
-    res.send();
+//sample middleware
+app.use((req,res) => {
+    console.log("Hii. I am middleware");
+    res.send("middleware finished.")
 });
+
+
+//home page
+app.get("/", (req,res) => {
+    res.send("Hi I am Root");
+});
+
+//random midlleware
+app.use("/random", (req,res) => {
+    res.send("this is a random page!!");
+ });
+
+
 app.listen(8080, () => {
     console.log("server listening to post 8080");
 });
