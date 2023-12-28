053099442ac220c248ed21255cfe832a1c26f1c4
RajiaRani <rajiarani.syal@gmail.com>
2023-12-28 11:10:27 -0600

    added app.js file

--- MIDDLEWARE/app.js
+++ MIDDLEWARE/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+
+app.get("/", (req,res) => {
+    res.send();
+});
 app.listen(8080, () => {
     console.log("server listening to post 8080");
 });
