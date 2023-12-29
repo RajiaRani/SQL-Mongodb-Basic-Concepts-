adca19a76d9dff3d39544d8c68a4f4d252e523cc
RajiaRani <rajiarani.syal@gmail.com>
2023-12-28 19:22:39 -0600

    added new errHandle.js file

--- MIDDLEWARE/errHandle.js
+++ MIDDLEWARE/errHandle.js
@@ -3,3 +3,19 @@ const app = express();
 
 //---------------- Express Default Error-Handler -------------
 
+const checkToken = (req,res,next) => {
+    let {token} = req.query;
+    if(token == "giveaccess"){
+        next();
+    };
+
+    throw new Error ("Acess Denied");
+};
+
+app.get("/api", checkToken,(req,res) => {
+  res.send("data");
+});
+
+app.listen(8080, () => {
+ console.log("i am lsitening on port 8080");
+});
