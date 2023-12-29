4f53f8b912c0849b5a5f7174e4b52338a9e70409
RajiaRani <rajiarani.syal@gmail.com>
2023-12-28 22:41:41 -0600

    added new errHandle.js file

--- MIDDLEWARE/errHandle.js
+++ MIDDLEWARE/errHandle.js
@@ -3,6 +3,22 @@ const app = express();
 
 //---------------- Express Default Error-Handler -------------
 
+
+app.get("/error", (req,res) => {
+    abcd = abcd;
+});
+app.use((err, req, res, next) => {
+    //console.log(err);
+    console.log("--------ERROR--------");
+    next(err);
+});
+
+app.use((err,req,res,next) => {
+    console.log("------ Error 2 -------");
+    next(err);
+});
+
+
 const checkToken = (req,res,next) => {
     let {token} = req.query;
     if(token == "giveaccess"){
