48083442642e86d567c8d4962d96722c83da75ff
RajiaRani <rajiarani.syal@gmail.com>
2023-12-29 22:30:09 -0600

    adde new MONGO3 folder

--- MONGO3/index.js
+++ MONGO3/index.js
@@ -20,7 +20,7 @@ main()
 });
 
 async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+    await mongoose.connect("mongodb://127.0.0.1:27017/fakewhatsapp");
 }
 
 //link the style
